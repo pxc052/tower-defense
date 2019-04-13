05983a7747f0f2628c48213645b6b004058958fa
chien <chenpx@emg.vn>
2019-04-13 10:33:28 +0700

    Laser Effect

--- Tower Defense/Assets/Scripts/Turret.cs
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -18,6 +18,8 @@ public class Turret : MonoBehaviour
     [Header("Use Laser")]
     public bool useLaser = false;
     public LineRenderer lineRenderer;
+    public ParticleSystem impactEffect;
+    public Light impactLight;
 
     [Header("Unity Setup Fields")]
     public Transform partToRotate;
@@ -42,6 +44,8 @@ public class Turret : MonoBehaviour
                 if (lineRenderer.enabled)
                 {
                     lineRenderer.enabled = false;
+                    impactEffect.Stop();
+                    impactLight.enabled = false;
                 }
             }
             return;
@@ -77,9 +81,16 @@ public class Turret : MonoBehaviour
         if (!lineRenderer.enabled)
         {
             lineRenderer.enabled = true;
+            impactEffect.Play();
+            impactLight.enabled = true;
         }
         lineRenderer.SetPosition(0, firePoint.position);
         lineRenderer.SetPosition(1, target.position);
+
+        Vector3 direction = firePoint.position - target.position;
+        impactEffect.transform.position = target.position + direction.normalized;
+        impactEffect.transform.rotation = Quaternion.LookRotation(direction);
+
     }
 
     void Shoot()
